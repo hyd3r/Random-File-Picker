b2150fd2796458561be871870a992423be1ef944
hyd3r <hyd3r.exe@gmail.com>
2019-12-28 22:21:53 +0800

    Added image and video support

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderSelect = new System.Windows.Forms.Button();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.randomizeButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderSelect
+            // 
+            this.folderSelect.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.folderSelect.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.folderSelect.Location = new System.Drawing.Point(0, 662);
+            this.folderSelect.MinimumSize = new System.Drawing.Size(101, 50);
+            this.folderSelect.Name = "folderSelect";
+            this.folderSelect.Size = new System.Drawing.Size(1113, 50);
+            this.folderSelect.TabIndex = 4;
+            this.folderSelect.TabStop = false;
+            this.folderSelect.Text = "Select Folder";
+            this.folderSelect.UseVisualStyleBackColor = true;
+            this.folderSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.fileNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileNameLabel.Location = new System.Drawing.Point(0, 558);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(1113, 44);
+            this.fileNameLabel.TabIndex = 1;
+            this.fileNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // randomizeButton
+            // 
+            this.randomizeButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.randomizeButton.Enabled = false;
+            this.randomizeButton.Location = new System.Drawing.Point(0, 632);
+            this.randomizeButton.Name = "randomizeButton";
+            this.randomizeButton.Size = new System.Drawing.Size(1113, 30);
+            this.randomizeButton.TabIndex = 1;
+            this.randomizeButton.TabStop = false;
+            this.randomizeButton.Text = "Randomize";
+            this.randomizeButton.UseVisualStyleBackColor = true;
+            this.randomizeButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1113, 558);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openButton
+            // 
+            this.openButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.openButton.Enabled = false;
+            this.openButton.Location = new System.Drawing.Point(0, 602);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(1113, 30);
+            this.openButton.TabIndex = 3;
+            this.openButton.TabStop = false;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1113, 712);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.randomizeButton);
+            this.Controls.Add(this.folderSelect);
+            this.Name = "Form1";
+            this.Text = "Random File Picker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button folderSelect;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Button randomizeButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button openButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using ErikE.Shuriken;
+using Microsoft.WindowsAPICodePack.Shell;
+using NReco.VideoConverter;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +17,120 @@ namespace Random_File_Picker
 {
     public partial class Form1 : Form
     {
+        DirectoryInfo selectedDir;
+        FolderSelectDialog folder;
+        FileInfo[] files;
+        Random randomIndex = new Random();
+        FFMpegConverter ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+        Stream stream;
+        Timer t = new Timer();
+        int currentFileIndex;
+        int frameCounter=1;
+        String[] video =
+        {
+            ".mp4",".webm",".mkv",".flv",".avi",".wmv",".m4v",".mpg",".mpeg",".wav",".mov",".webm",".3gp"
+        };
+        String[] image =
+        {
+           ".JPG",".JPEG",".GIF",".BMP",".PNG",".WebP"
+        };
+
         public Form1()
         {
             InitializeComponent();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(t_Tick);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+            folder = new FolderSelectDialog();
+            if (folder.Show())
+            {
+                selectedDir = new DirectoryInfo(folder.FileName);
+                files = selectedDir.GetFiles();
+                fileNameLabel.Focus();
+                if (files.Length == 0){
+                    MessageBox.Show("Doesn't contain any files");
+                    randomizeButton.Enabled = false;
+                }
+                else randomizeButton.Enabled = true;
+                openButton.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+            openButton.Enabled = true;
+            fileNameLabel.Focus(); fileNameLabel.Focus();
+            int rnd = randomIndex.Next(0, files.Length-1);
+
+            while (files[rnd].Attributes.HasFlag(FileAttributes.Hidden))
+            {
+                rnd = randomIndex.Next(0, files.Length);
+            }
+            string format = checkExtension(files[rnd].Extension);
+            if (format.Equals("video"))
+            {
+                currentFileIndex = rnd;
+                fileNameLabel.Text = files[currentFileIndex].Name;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                t.Start();
+                frameCounter = 1;
+
+            }
+            else if (format.Equals("image"))
+            {
+                currentFileIndex = rnd;
+                fileNameLabel.Text = files[currentFileIndex].Name;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = Image.FromFile(files[currentFileIndex].FullName);
+            }
+            else
+            {
+                currentFileIndex = rnd;
+                fileNameLabel.Text = files[currentFileIndex].Name;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                ShellFile shellFile = ShellFile.FromFilePath(files[currentFileIndex].FullName);
+                Bitmap shellThumb = shellFile.Thumbnail.ExtraLargeBitmap;
+                pictureBox1.Image = shellThumb;
+            }
+
+        }
+        void t_Tick(object sender, EventArgs e)
+        {
+            stream = new MemoryStream();
+            ffMpeg.GetVideoThumbnail(files[currentFileIndex].FullName, stream, frameCounter);
+            if (stream.Length == 0)
+            {
+                frameCounter = 1;
+            }
+            else
+            {
+                pictureBox1.Image = Image.FromStream(stream);
+            }
+            frameCounter += new Random().Next(1, 1000);
+
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            fileNameLabel.Focus();
+            t.Stop();
+            Process.Start(files[currentFileIndex].FullName);
+        }
+
+        private string checkExtension(string ext)
+        {
+            if(video.Any(s => s.IndexOf(ext, StringComparison.CurrentCultureIgnoreCase) > -1))
+                return "video";
+            else if(image.Any(s => s.IndexOf(ext, StringComparison.CurrentCultureIgnoreCase) > -1))
+                return "image";
+            else return "";
+
         }
     }
+
 }
